242861ddbdd2839bd3a7e31bc93a28d02c5784f2
Yamil <yamrreca@ciencias.unam.mx>
2024-10-27 18:12:31 -0600

    refactor: move the basic autocommands to their own file

--- init.lua
+++ init.lua
@@ -1,19 +1,6 @@
 require 'basic-options'
 require 'basic-keymaps'
-
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
+require 'basic-autocommands'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 -- See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
